a2a91fef5a12bcc1e4331fbbb29b73a399a8a10f
Francisco González <francisco.gonzalez@enterprisedb.com>
2021-11-06 14:29:56 -0600

    update fork alias

--- zshrc.d/aliases.bash
+++ zshrc.d/aliases.bash
@@ -16,6 +16,6 @@ alias brewup='brew update; brew upgrade; brew cleanup; brew doctor; brew bundle'
 
 alias git-tree='tree = log --graph --decorate --pretty=oneline --abbrev-commit'
 
-alias fork="open -a fork"
+alias forkapp="open -a fork"
 
 alias npm-global="npm ls -g --depth=0"
