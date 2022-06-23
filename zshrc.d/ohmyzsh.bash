a2a78ba99c4b2a77a86e9175431dcd4c7fa8372c
Francisco González <francisco.gonzalez@enterprisedb.com>
2022-06-23 14:25:11 -0500

    chore

--- zshrc.d/ohmyzsh.bash
+++ zshrc.d/ohmyzsh.bash
@@ -10,6 +10,7 @@ ZSH_DISABLE_COMPFIX="true"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+# ZSH_THEME="af-magic"
 ZSH_THEME="avit"
 
 # Set list of themes to pick from when loading at random
