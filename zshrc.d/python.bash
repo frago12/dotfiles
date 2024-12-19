04beb967dc2d08b131dfb086acce0876383fc61c
Francisco González <francisco@joinforage.com>
2024-12-19 17:47:14 -0600

    update python management

--- zshrc.d/python.bash
+++ zshrc.d/python.bash
@@ -1,1 +1,4 @@
-alias python=/usr/bin/python3
+# alias python=/usr/bin/python3
+# alias PYTHON=/Users/franciscogonzalez/.pyenv/shims/python2.7
+# alias python=/Users/franciscogonzalez/.pyenv/shims/python2.7
+export PATH="${HOME}/.pyenv/shims:${PATH}"
