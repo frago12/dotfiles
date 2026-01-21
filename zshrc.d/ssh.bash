94445fc039002f731c574d980cef96148f36231e
Francisco González <francisco@frago.dev>
2026-01-21 01:11:20 -0600

    update how ssh is handled

--- zshrc.d/ssh.bash
+++ zshrc.d/ssh.bash
@@ -1,6 +1,23 @@
-for possiblekey in ${HOME}/.ssh/id_*; do
-    if grep -q PRIVATE "$possiblekey"; then
-        ssh-add -K "$possiblekey" &> /dev/null
-    fi
-done
+# SSH key management is handled by ~/.ssh/config
+#
+# The following options are configured there:
+#   AddKeysToAgent yes  - Automatically add keys to ssh-agent on first use
+#   UseKeychain yes     - Store/retrieve passphrases from macOS Keychain
+#   IdentityFile        - Specifies which key(s) to use
+#
+# This approach replaces the old ssh-add loop that ran on every shell startup.
+#
+# Here's an example
+#
+#---
+# ~/.ssh/config
+#---
+# Host *
+#     AddKeysToAgent yes
+#     UseKeychain yes
+#     IdentityFile ~/.ssh/id_ed25519
 
+# Host github.com-personal
+#     HostName github.com
+#     User git
+#     IdentityFile ~/.ssh/id_ed25519_personal
